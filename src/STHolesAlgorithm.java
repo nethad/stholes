1da9340187981e4ffc66d5794065a19b3f5bce5e
bebop3 <niki_@gmx.ch>
2011-11-16 15:42:53 +0100

    created bucket unit tests. & Start of bucket identification

--- src/STHolesAlgorithm.java
+++ src/STHolesAlgorithm.java
@@ -1,22 +1,51 @@
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class STHolesAlgorithm {
 
     private final Bucket rootBucket;
     private int maxBucketCount;
 
-    public STHolesAlgorithm() {
-        rootBucket = new Bucket(0.0, 1.0, 0.0, 1.0);
+    /**
+     * Creates a new StHoles Algorithm.
+     * @param maxBucketCount The number of buckets the algorithm may create.
+     */
+    public STHolesAlgorithm(int maxBucketCount) {
+        this.maxBucketCount = maxBucketCount;
+        
+    	// Create a new root bucket over the complete range and initialize f(root) with 0.
+        this.rootBucket = new Bucket(0.0, 1.0, 0.0, 1.0);
     }
 
-    public double getEstimateQuery(Query query) {
-        return rootBucket.getEstimateForQuery(query);
+    /**
+     * Estimates the query.
+     * @param q The query.
+     * @return The estimate of the query.
+     */
+    public double getEstimateQuery(Query q) {
+        return rootBucket.getEstimateForQuery(q);
     }
 
-    public void updateHistogram(Query query, int actualResultCount) {
+    public void updateHistogram(Query q, int actualResultCount) {
         // identify candidate holes
-
+    	Map<Bucket, Bucket> candiates = IdentifyCandiateHoles(q, actualResultCount);
+    	
         // drill candidate holes
 
         // merge superfluous buckets
     }
-
+    
+    private Map<Bucket, Bucket> IdentifyCandiateHoles(Query q, int actualResultCount)
+    {
+    	Map<Bucket, Bucket> candiates = new HashMap<Bucket, Bucket>();
+    	Rectangle2D.Double queryBox = q.getRectangle2D(); 
+  	  // The paper suggests that we have the actual count of all tuples in b.
+  	  // Approximate tb by assuming the results are uniformly distributed across q.
+  	  //int tb = actualResultCount * (int)(getVolume()/(queryBox.getHeight()*queryBox.getWidth()));
+  	  
+    	return candiates;    
+    }
 }
--- test/BucketTest.java
+++ test/BucketTest.java
@@ -1,36 +1,128 @@
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Test the histogram volume and estimate functions.
+ * See "DB Impl Scenarios" document for used scenarios.
+ */
 public class BucketTest {
+	
+	// Round estimate results to 6 decimals 
+	private final static double VOLUME_PRECISION = 1000000;
 
-    private Bucket rootBucketSingle;
-    private Bucket rootBucket;
-
-    @Before
-    public void setup() {
-        rootBucketSingle = new Bucket(0.0, 1.0, 0.0, 1.0);
-        rootBucket = new Bucket(0.0, 1.0, 0.0, 1.0);
-        Bucket childBucket = new Bucket(0.5, 1.0, 0.5, 1.0);
-        rootBucket.addChildBucket(childBucket);
-    }
-
+	// Round estimate results to 2 decimals 
+	private final static double ESTIMATE_PRECISION = 100;
+	
+	private static double RoundedVolume(double result)
+	{
+		return Math.round(result * VOLUME_PRECISION) / VOLUME_PRECISION;
+	}
+	
+	private static double RoundedEstimate(double result)
+	{
+		return Math.round(result * ESTIMATE_PRECISION) / ESTIMATE_PRECISION;
+	}
+    
     @Test
-    public void testVolume_forSingleFullBucket() {
-        assertThat(rootBucketSingle.getVolume(), is(1.0));
+    public void testHistogram_1()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram1();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	assertThat(RoundedVolume(root.getVolume()), is(1.0));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(600.0));
+    	
+    	/*
+    	Bucket candiateR = root.IdentifyCandiate(q, 200);
+    	assertThat(RoundedVolume(candiateR.getVolume()), is(0.6));
+    	assertThat(candiateR.getFrequency(), is(200));
+    	*/
     }
-
+    
     @Test
-    public void testVolume_singleBucketWithChild() {
-        assertThat(rootBucket.getVolume(), is(0.75));
+    public void testHistogram_2()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram2();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	
+    	assertThat(RoundedVolume(root.getVolume()), is(0.68));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(604.41));
+    	
+    	/*
+    	Bucket candiateR = root.IdentifyCandiate(q, 200);
+    	assertThat(RoundedVolume(candiateR.getVolume()), is(0.6));
+    	assertThat(candiateR.getFrequency(), is(200));
+    	*/
     }
-
+    
     @Test
-    public void testEstimateForQuery() {
-        Query query = new Query(0.3, 0.7, 0.0, 1.0);
-        rootBucket.getEstimateForQuery(query);
+    public void testHistogram_3()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram3();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	
+    	assertThat(RoundedVolume(root.getVolume()), is(0.68));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(942.75));
     }
-
+    
+    @Test
+    public void testHistogram_4()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram4();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	
+    	assertThat(RoundedVolume(root.getVolume()), is(0.68));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(8029.41));
+    }
+    
+    @Test
+    public void testHistogram_5()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram5();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	
+    	assertThat(RoundedVolume(root.getVolume()), is(0.92));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(886.96));
+    }
+    
+    @Test
+    public void testHistogram_6()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram6();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	
+    	assertThat(RoundedVolume(root.getVolume()), is(0.9));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(900.0));
+    }
+    
+    @Test
+    public void testHistogram_7()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram7();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	
+    	assertThat(RoundedVolume(root.getVolume()), is(0.68));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(1006.33));
+    }
+    
+    @Test
+    public void testHistogram_8()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram8();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	
+    	assertThat(RoundedVolume(root.getVolume()), is(0.84));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(623.81));
+    }
+    
+    @Test
+    public void testHistogram_9()
+    {
+    	Bucket root = HistorgramFactory.CreateHistogram9();
+    	Query q = HistorgramFactory.CreateTestQuery();
+    	
+    	assertThat(RoundedVolume(root.getVolume()), is(0.84));
+    	assertThat(RoundedEstimate(root.getEstimateForQuery(q)), is(714.29));
+    }
 }
