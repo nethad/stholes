b7daeb11f8e927eefddb9325a690aa65e6c96c65
bebop3 <niki_@gmx.ch>
2011-11-16 15:49:30 +0100

    Failed merge fixed

--- src/Query.java
+++ src/Query.java
@@ -2,32 +2,13 @@ import java.awt.geom.Rectangle2D;
 
 public class Query {
 
-<<<<<<< HEAD
-    private final double xMin, xMax, yMin, yMax;
-
-    public Query(double xMin, double xMax, double yMin, double yMax) {
-        this.xMin = xMin;
-        this.xMax = xMax;
-        this.yMin = yMin;
-        this.yMax = yMax;
-    }
-
-    @Override
-    public String toString() {
-        return "[xMin: " + xMin + ",xMax: " + xMax + ",yMin: " + yMin + ",yMax: " + yMax + "]";
-    }
-
-    public Rectangle2D.Double getRectangle2D() {
-        return new Rectangle2D.Double(xMin, yMin, xMax - xMin, yMax - yMin);
-    }
-=======
 	private final double xMin, xMax, yMin, yMax;
 
 	public Query(double xMin, double xMax, double yMin, double yMax) {
 		this.xMin = xMin;
 		this.xMax = xMax;
 		this.yMin = yMin;
-		this.yMax = xMax;
+		this.yMax = yMax;
 	}
 
 	@Override
@@ -55,5 +36,4 @@ public class Query {
 	public double getYMax() {
 		return yMax;
 	}
->>>>>>> 209c2f3e74e601747774d098a52c323739d6cd50
 }
