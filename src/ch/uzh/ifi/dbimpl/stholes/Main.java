7927bafd3e47349e1f629e9898162d0e6d809ba4
Thomas R <nethad@gmx.net>
2011-11-17 22:23:38 +0100

    wire classes to interfaces in main

--- src/ch/uzh/ifi/dbimpl/stholes/Main.java
+++ src/ch/uzh/ifi/dbimpl/stholes/Main.java
@@ -1,4 +1,5 @@
 package ch.uzh.ifi.dbimpl.stholes;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,39 +10,43 @@ import ch.uzh.ifi.dbimpl.stholes.data.Query;
 
 public class Main {
 
-    public static void main(String[] args) {
-        doDBStuff();
+	public static void main(String[] args) {
+		// doDBStuff();
 
-        QueryGenerator queryGenerator = null;
-        STHolesAlgorithm stHolesAlgorithm = new STHolesAlgorithm(1000);
-        Database database = null;
+		QueryGenerator queryGenerator = new RandomQueryGenerator();
+		STHolesAlgorithm stHolesAlgorithm = new STHolesAlgorithm(1000);
+		Database database = new DefaultDatabase("db/random");
 
-        for (int i = 0; i < 1000 && queryGenerator.hasNextQuery(); i++) {
-            Query query = queryGenerator.nextQuery();
-            int actualResultCount = database.executeCountQuery(query);
-            double estimatedCount = stHolesAlgorithm.getEstimateQuery(query);
-            stHolesAlgorithm.updateHistogram(query, actualResultCount);
-            System.out.println(query.toString() + "; estimated count: " + estimatedCount + ", actual count: "
-                    + actualResultCount);
-        }
+		for (int i = 0; i < 1000 && queryGenerator.hasNextQuery(); i++) {
+			Query query = queryGenerator.nextQuery();
+			int actualResultCount = database.executeCountQuery(query);
+			double estimatedCount = stHolesAlgorithm.getEstimateQuery(query);
+			stHolesAlgorithm.updateHistogram(query, actualResultCount);
+			System.out.println(query.toString() + "; estimated count: "
+					+ estimatedCount + ", actual count: " + actualResultCount);
+		}
 
-    }
+	}
 
-    private static void doDBStuff() {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:hsqldb:file:testdb;ifexists=true", "SA", "");
-            Statement statement = connection.createStatement();
-            statement.execute("create table test (id integer primary key, x double, y double);");
-            statement.executeUpdate("insert into test (id, x, y) values (1, 0.5, 0.5);");
-            ResultSet result = statement.executeQuery("select count(*) from test;");
-            if (result.next()) {
-                System.out.println("count = " + result.getInt(1));
-            } else {
-                System.err.println("No result.");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	private static void doDBStuff() {
+		try {
+			Connection connection = DriverManager.getConnection(
+					"jdbc:hsqldb:file:testdb;ifexists=true", "SA", "");
+			Statement statement = connection.createStatement();
+			statement
+					.execute("create table test (id integer primary key, x double, y double);");
+			statement
+					.executeUpdate("insert into test (id, x, y) values (1, 0.5, 0.5);");
+			ResultSet result = statement
+					.executeQuery("select count(*) from test;");
+			if (result.next()) {
+				System.out.println("count = " + result.getInt(1));
+			} else {
+				System.err.println("No result.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
